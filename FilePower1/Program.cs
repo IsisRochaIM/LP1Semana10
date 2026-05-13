b1e725c4d3af887b9476ff51571e2ff6a7bf0b45
Isis <mejaikatwt@gmail.com>
2026-05-13 16:41:10 +0100

    ex 5 incompleto

---
+++ FilePower1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Queue<string> files = new Queue<string>();
+            string fileName = args[0];
+            File.Create(fileName);
+            
+            while(true)
+            {
+                string inp = Console.ReadLine();
+                if(inp == "")
+                {
+                    break;
+                }
+                files.Enqueue(inp);
+            }
+            string path;
+            path = $"C:\\LPSemana10\\{fileName}";
+            Console.WriteLine($"Path: {path}");
+            File.WriteAllLines(path, files);
+
+        }
+    }
+}
